66a198b056966170272d87abf17cee347f19cf90
Rudolph <rudoljus@berkeleyprep.org>
2018-10-11 10:50:34 -0400

    thanks

--- Homework4.java
+++ Homework4.java
@@ -1,10 +1,15 @@
 public class Homework4 {
 
+// Justin Rudolph
 	/* Finish the constructor and create any necessary instance
 	 * variables
 	 */
-	public Homework4(String s, int i) {
+	 private String s;
+	 private int i;
 
+	public Homework4(String s, int i) {
+		this.s = s;
+		this.i = i;
 	}
 
 	/* Return the product of the stored number and the
@@ -12,6 +17,8 @@ public class Homework4 {
 	 * multiplication
 	 */
 	public int multiply(double d) {
+		int multiply = (int) (i * d);
+		return multiply;
 
 	}
 
@@ -19,26 +26,31 @@ public class Homework4 {
 	 * product by the stored number
 	 */
 	public double sumProduct(int a, double b) {
-
+		double sumProduct = ((double) a + b) * i;
+		return sumProduct;
 	}
 
 	/* Return the remainder of the first number divided by
 	 * the integer portion of the provided double
 	 */
 	 public int remainder(double d) {
-
+		 int remainder =  i % (int) d;
+		 return remainder;
 	}
 
 	/* Return the length of the stored String
 	 */
 	public int getLength() {
-
+		int getLength = s.length();
+		return getLength;
 	}
 
 	/* Return the stored String followed by the provided
 	 * String. Put a space between the two Strings
 	 */
 	public String concat(String s) {
+		String concat = this.s + " " + s;
+		return concat;
 
 	}
 
@@ -47,14 +59,14 @@ public class Homework4 {
 
 		Homework4 hw4 = new Homework4("Hello, World!", 5);
 
-		if (hw4.multiply(2.5) == 12.25) {
+		if (hw4.multiply(2.5) == 12.0) {
 			System.out.println("Pass 1");
 		} else {
 			System.out.println("Fail 1");
 			exitCode += 1;
 		}
 
-		if (hw4.sumProduct(2, 2.5) == 20.25) {
+		if (hw4.sumProduct(2, 2.5) == 22.5) {
 			System.out.println("Pass 2");
 		} else {
 			System.out.println("Fail 2");
